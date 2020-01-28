f399059e2c57735ac4b5dc35e9d32731f5e8c56c
Viktoriia Vasyltsiv <Viktoriia.Vasyltsiv.KN.2016@lpnu.ua>
2020-01-28 19:36:10 +0200

    added create page

--- BookStoreClient/src/app/components/new-book/new-book.component.ts
+++ BookStoreClient/src/app/components/new-book/new-book.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-book',
@@ -7,9 +10,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewBookComponent implements OnInit {
 
-  constructor() { }
+  addBookForm: FormGroup;
+
+  constructor(private service: BookService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+    this.addBookForm = this.fb.group({
+      //id:[Math.floor(Math.random()*1000)],
+      title:[null, Validators.required],
+      authorName:[null, Validators.required],
+      publisherName:[null, Validators.compose([Validators.required, Validators.minLength(30)])],
+      price:[null],
+      publicationYear:[null]
+    })
   }
 
+  onSubmit(){
+    this.service.addBook(this.addBookForm.value).subscribe( () => {
+      this.router.navigate(["/books"]);
+    })
+  }
 }
+// let tour = automapper.map(
+//   'TourFormModel',
+//   'TourWithManagerForCreation',
+//   this.tourForm.value);
+// this.tourService.addTourWithManager(tour)
+//   .subscribe(
+//     () => {
+//       this.router.navigateByUrl('/tours');
+//     },
+//     (validationResult) => 
+//     { ValidationErrorHandler.handleValidationErrors(this.tourForm, validationResult); });
