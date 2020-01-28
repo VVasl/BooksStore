58c186ec985e301d0631c9e66648be42f40d33d1
Viktoriia Vasyltsiv <Viktoriia.Vasyltsiv.KN.2016@lpnu.ua>
2020-01-28 19:33:56 +0200

    added books page

--- BookStoreClient/src/app/components/books/books.component.ts
+++ BookStoreClient/src/app/components/books/books.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { BookService } from 'src/app/services/book.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-books',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BooksComponent implements OnInit {
 
-  constructor() { }
+  //public books: Book[] =[{
+    //title: 'First',
+    //authorName:'John'
+  //},{
+    //title:'Second',
+    //authorName:'Kevin'
+  //}];
+  public books: Book[];
+  
+  constructor(private service: BookService, private router: Router) { }
 
   ngOnInit() {
+    this.service.getAllBooks().subscribe(data => {
+      this.books = data;
+    })
+  }
+
+  showBook(id: number){
+    this.router.navigate(["/show-book/"+id]);
+  }
+
+  updateBook(id: number){
+    this.router.navigate(["/update-book/"+id]);
+  }
+
+  deleteBook(id: number){
+    this.router.navigate(["/delete-book/"+id]);
   }
 
 }
---
+++ BookStoreClient/src/app/shared/book.ts
@@ -0,0 +1,9 @@
+interface Book
+{
+    id: number;
+    title: string;
+    authorName: string;
+    publisherName: string;
+    price: number;
+    publicationYear: number;
+}
