00d35738d30d1ac91ecce2bef3bd1d47a41e5760
Viktoriia Vasyltsiv <Viktoriia.Vasyltsiv.KN.2016@lpnu.ua>
2020-01-29 18:35:54 +0200

    add orderItems and orders controller

---
+++ BooksStore/Controllers/OrderItemsController.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using BooksStore.Data;
+using System.Threading.Tasks;
+using AutoMapper;
+using NLog;
+using BooksStore.Data.Entities;
+using BooksStore.Models;
+
+namespace BooksStore.Controllers
+{
+    [Route("/api/orders/{orderId}/items")]
+   // [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class OrderItemsController : Controller
+    {
+        private readonly IBooksStoreRepository _repository;
+        private readonly Logger _logger;
+        private readonly IMapper _mapper;
+
+        public OrderItemsController(IBooksStoreRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = LogManager.GetCurrentClassLogger();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get(int orderId)
+        {
+            try
+            {
+                // var order = _repository.GetOrderById(User.Identity.Name, orderId);
+                var order = _repository.GetOrderById(orderId);
+
+                if (order != null)
+                {
+                    return Ok(_mapper.Map<IEnumerable<OrderItem>, IEnumerable<OrderItemsDto>>(order.Items));
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Failed to get order items: {ex}");
+                return BadRequest("Failed to get order items");
+            }
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int orderId, int id)
+        {
+            try
+            {
+                //var order = _repository.GetOrderById(User.Identity.Name, orderId);
+                var order = _repository.GetOrderById(orderId);
+
+                if (order != null)
+                {
+                    var item = order.Items.Where(i => i.Id == id).FirstOrDefault();
+                    if (item != null)
+                    {
+                        return Ok(_mapper.Map<OrderItem, OrderItemsDto>(item));
+                    }
+                    else
+                    {
+                        return NotFound();
+                    }
+                }
+                else
+                {
+                    return NotFound();
+                }
+                /// too much NotFound 
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Failed to get order item:{ex}");
+                return BadRequest("Failed to get order item");
+            }
+        }
+    }
+}
---
+++ BooksStore/Controllers/OrdersController.cs
@@ -0,0 +1,145 @@
+﻿using AutoMapper;
+using BooksStore.Data;
+using BooksStore.Data.Entities;
+using BooksStore.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using NLog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksStore.Controllers
+{
+    [Route("api/[Controller]")]
+    //[Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    public class OrdersController : Controller
+    {
+        private readonly IBooksStoreRepository _repository;
+        private readonly Logger _logger;
+        private readonly UserManager<StoreUser> _userManager;
+        private readonly IMapper _mapper;
+
+        //public OrdersController(IBooksStoreRepository repository, IMapper mapper, UserManager<StoreUser> userManager)
+        //{
+        //    _repository = repository;
+        //    _logger = LogManager.GetCurrentClassLogger();
+        //    _mapper = mapper;
+        //    _userManager = userManager;
+        //}
+        public OrdersController(IBooksStoreRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _logger = LogManager.GetCurrentClassLogger();
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public IActionResult Get(bool includeItems = true)
+        {
+            try
+            {
+                var results = _repository.GetAllOrders(includeItems);
+                return Ok(_mapper.Map<IEnumerable<Order>, IEnumerable<OrderDto>>(results));
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Failed to get orders:{ex}");
+                return BadRequest("Failed to get orders");
+            }
+        }
+
+        [HttpGet("{id:int}")]
+        public IActionResult Get(int id)
+        {
+            try
+            {
+                //var order = _repository.GetOrderById(User.Identity.Name, id);
+                var order = _repository.GetOrderById(id);
+
+                if (order != null)
+                {
+                    return Ok(_mapper.Map<Order, OrderDto>(order));
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Failed to get order: {ex}");
+                return BadRequest("Failed to get order");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody]OrderDto model)
+        {
+            // add it to the db
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var newOrder = _mapper.Map<OrderDto, Order>(model);
+
+                    if (newOrder.OrderDate == DateTime.MinValue)
+                    {
+                        newOrder.OrderDate = DateTime.Now;
+                    }
+
+                    _repository.AddEntity(newOrder);
+                    if (_repository.SaveAll())
+                    {
+                        return Created($"/api/orders/{newOrder.Id}", _mapper.Map<Order, OrderDto>(newOrder));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.Error($"Failed to save a new order: {ex}");
+            }
+
+            return BadRequest("Failed to save new order");
+        }
+
+        //[HttpPost]
+        //public async Task<IActionResult> Post([FromBody] OrderDto model)
+        //{
+        //    try
+        //    {
+        //        if (ModelState.IsValid)
+        //        {
+        //            var newOrder = _mapper.Map<OrderDto, Order>(model);
+
+        //            if (newOrder.OrderDate == DateTime.MinValue)
+        //            {
+        //                newOrder.OrderDate = DateTime.Now;
+        //            }
+
+        //            var currentUser = await _userManager.FindByNameAsync(User.Identity.Name);
+        //            newOrder.User = currentUser;
+
+        //            _repository.AddEntity(newOrder);
+        //            if (_repository.SaveAll())
+        //            {
+        //                return Created($"/api/orders/{newOrder.Id}", _mapper.Map<Order, OrderDto>(newOrder));
+        //            }
+        //        }
+        //        else
+        //        {
+        //            return BadRequest(ModelState);
+        //        }
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.Error($"Failed to save a new order: {ex}");
+        //    }
+
+        //    return BadRequest("Failed to save a new order");
+        //}
+    }
+}
