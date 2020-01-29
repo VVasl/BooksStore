d58071fb5cef5f821ca5eef71fe47c979ab71eaa
Viktoriia Vasyltsiv <Viktoriia.Vasyltsiv.KN.2016@lpnu.ua>
2020-01-29 18:27:56 +0200

    added order and orderItems models

---
+++ BooksStore/Models/OrderDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksStore.Models
+{
+    public class OrderDto
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        public string OrderNumber { get; set; }
+        public ICollection<OrderItemsDto> Items { get; set; }
+    }
+}
---
+++ BooksStore/Models/OrderItemsDto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BooksStore.Models
+{
+    public class OrderItemsDto
+    {
+        public int Id { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        public decimal UnitPrice { get; set; }
+        [Required]
+        public int BookId { get; set; }
+        public string BookCategory { get; set; }
+        public string BookTitle { get; set; }
+        public string BookAuthor { get; set; }
+        public string BookAuthorId { get; set; }
+    }
+}
